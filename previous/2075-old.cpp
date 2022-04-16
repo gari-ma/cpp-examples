67e2657e2582ead69dc83e866e8000e86dc1d28d
npgarima <102780848+npgarima@users.noreply.github.com>
2022-04-16 17:40:37 +0545

    2076no1

--- previous/2075-old.cpp
+++ previous/2075-old.cpp
@@ -1,4 +1,5 @@
 /*
- 8. Write a C++ program containing a possible exception. Use a try block to throw it and a catch block to handle it.
+ 8. Write a C++ program containing a possible exception.
+  Use a try block to throw it and a catch block to handle it.
 
  */
--- previous/2076-01.cpp
+++ previous/2076-01.cpp
@@ -1,12 +1,4 @@
-/* 
- Write a program according to the specification given below:
 
- Create a class Teacher with data members tid & subject and member functions for reading and displaying data members.
- Create another class Staff with data members sid & position, and member function for reading and displaying data members.
- Derive a class Coordinator from Teacher and Staff and the class must have its own data member department and member functions for reading and displaying data members.
- Create two object of Coordinator class and read and display their details.
- */
- 
  #include<iostream>
  using namespace std;
  
