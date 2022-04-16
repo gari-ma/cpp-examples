c2368f3f7a719e26ee1b8b2e40881888934615a8
npgarima <102780848+npgarima@users.noreply.github.com>
2022-04-16 14:50:39 +0545

    Addec 2076-02

--- previous/2076-02.cpp
+++ previous/2076-02.cpp
@@ -1,1 +1,36 @@
 /* . Write programs to add two object of distance class with data members feet and inch by using by using member function and friend function. */
+
+#include<iostream>
+
+using namespace std;
+
+class Distance {
+    public:
+    int feet,inch;
+    
+    public:
+    Distance(int feet, int inch) {
+        this->feet = feet;
+        this->inch = inch;
+    }
+    
+    Distance operator+(Distance d1){
+        return Distance(feet + d1.feet ,inch + d1.inch);
+    } 
+    
+    
+};
+
+
+int main() {
+    Distance dis1(10,11);
+    Distance dis2(20,52);
+    Distance dis3 = dis1 + dis2;
+    
+    cout<<"Distance dis3:"<<endl;
+    cout<<"feet: "<<dis3.feet<<endl;
+    cout<<"inch: "<<dis3.inch<<endl;
+    
+    
+    return 0;
+}
