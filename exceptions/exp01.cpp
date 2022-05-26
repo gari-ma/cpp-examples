d7c98f29687a3f696df8914bb0f5a549d0a2218a
Sulav Parajuli <impsike@pop-os.localdomain>
2022-05-26 22:41:25 +0545

    Added Codes new

--- exceptions/exp01.cpp
+++ exceptions/exp01.cpp
@@ -9,12 +9,17 @@ int main()
     {
         cin>>a;
         if(a == 0) throw string("Error");
+        if(a == 1) throw a;
+        
         cout<<"A is: "<<a<<endl;
 
     }
     catch (string s)
     {
         cout<<s<<endl;
+    }
+    catch(int a) {
+        cout<<"a is 1"<<endl;
     }
 
     return 0;
